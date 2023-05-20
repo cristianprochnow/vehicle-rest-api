aa8839ddfc6fb02e3c8a1ca667ff6f1dc2397b6e
Cristian Prochnow <cristianprochnow@gmail.com>
2023-05-20 20:18:00 -0300

    fix: change localhost address

--- src/config/const.js
+++ src/config/const.js
@@ -1,6 +1,6 @@
 export const DB_NAME = 'VEHICLES';
 export const DB_USER = 'root';
 export const DB_PASS = 'root';
-export const DB_HOST = 'localhost';
+export const DB_HOST = '127.0.0.1';
 export const DB_PORT = '3306';
 export const SERVER_PORT = 3000;
